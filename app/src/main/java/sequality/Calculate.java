f5f9de636f9f19041fc13e6714d6f14f111bf5c9
m23a20 <m1m23a20@st.oit.ac.jp>
2023-06-26 10:03:22 +0900

    メソッドを追加

--- app/src/main/java/sequality/Calculate.java
+++ app/src/main/java/sequality/Calculate.java
@@ -1,7 +1,21 @@
 package sequality;
 
+import java.util.stream.IntStream;
+
 public class Calculate {
 	public int sum(int x, int y) {
 		return x + y;
+	}
+
+	public double average(int x, int y) {
+		return sum(x, y) / 2;
+	}
+
+	public int rangeSum(int start, int end) {
+		return IntStream.rangeClosed(start, end).sum();
+	}
+
+	public double rangeAverage(int start, int end) {
+		return IntStream.rangeClosed(start, end).average().orElse(0);
 	}
 }
