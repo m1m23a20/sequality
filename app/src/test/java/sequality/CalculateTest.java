c25cec8dd98a079f690ec34344405ac0070d67b9
m23a20 <m1m23a20@st.oit.ac.jp>
2023-06-26 10:51:51 +0900

    テストを追加

--- app/src/test/java/sequality/CalculateTest.java
+++ app/src/test/java/sequality/CalculateTest.java
@@ -11,4 +11,28 @@ public class CalculateTest {
 		int expected = 5;
 		assertEquals(expected, calculate.sum(2, 3));
 	}
+
+	@Test
+	public void testAverage() {
+		Calculate calculate = new Calculate();
+		assertEquals("2.5", String.format("%.1f", calculate.average(2, 3)));
+	}
+
+	@Test
+	public void testRangeSum() {
+		Calculate calculate = new Calculate();
+		assertEquals(5050, calculate.rangeSum(1, 100));
+	}
+
+	@Test
+	public void testRangeAverage() {
+		Calculate calculate = new Calculate();
+		assertEquals("50.5", String.format("%.1f", calculate.rangeAverage(1, 100)));
+	}
+
+	@Test
+	public void testRangeSumWithCondition() {
+		Calculate calculate = new Calculate();
+		assertEquals(25, calculate.rangeSum(1, 10, num -> num % 2 == 1));
+	}
 }
